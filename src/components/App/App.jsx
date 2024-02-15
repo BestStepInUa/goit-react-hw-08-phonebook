c7971b1cc7ae7abbd1efd8d2feba4c5be26e7e01
BestStepInUa <beststep.in.ua@gmail.com>
2024-02-15 14:50:54 +0200

    weird div

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,24 @@
 import { Toaster } from 'react-hot-toast';
-import AddContactForm from 'components/AddContactForm';
-import ContactsList from 'components/ContactsList';
-import ContactsFilter from 'components/ContactsFilter';
+// import AddContactForm from 'components/AddContactForm';
+// import ContactsList from 'components/ContactsList';
+// import ContactsFilter from 'components/ContactsFilter';
 import AppContainer from './App.styled';
+import RegistrationPage from 'pages/RegistrationPage';
+import Header from 'components/Header';
 
 const App = () => {
   return (
-    <AppContainer>
-      <h1>Phonebook</h1>
+    <>
+      <Header />
+      <AppContainer>
+        {/* <h1>Phonebook</h1>
       <AddContactForm />
       <ContactsFilter />
-      <ContactsList />
-      <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
-    </AppContainer>
+      <ContactsList /> */}
+        {/* <RegistrationPage /> */}
+        <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
+      </AppContainer>
+    </>
   );
 };
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import { CenterFocusStrong } from '@mui/icons-material';
+// import ContactsIcon from '@mui/icons-material/Contacts';
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar sx={{ height: '32px' }}>
+        <Typography
+          component="h1"
+          fontFamily="Helvetica Neue"
+          fontSize="40px"
+          fontStyle="italic"
+          lineHeight="32px"
+          variant="overline"
+          align="left"
+          color="pink"
+          display="flex"
+          columnGap="15px"
+          alignItems="center"
+        >
+          My phone book{' '}
+          <ContactPhoneIcon fontSize="large" sx={{ color: '#fff' }} />
+        </Typography>
+        {/* <IconButton>
+          <ContactsIcon />
+        </IconButton> */}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
