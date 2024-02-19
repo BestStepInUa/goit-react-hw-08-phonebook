a27a1552ff88eef3332d657ba1793633a0d9ad5a
BestStepInUa <beststep.in.ua@gmail.com>
2024-02-19 09:55:00 +0200

    redux-persist added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/auth-slice';
 import { contactsReducer } from './contacts/contacts-slice';
 import { filterReducer } from './filter/filter-slice';
 
+const persistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+const rootReducer = combineReducers({
+  auth: persistedAuthReducer,
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    contacts: contactsReducer,
-    filter: filterReducer,
+  reducer: rootReducer,
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
   },
 });
+
+export const persistor = persistStore(store);
